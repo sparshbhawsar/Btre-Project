2ea2aa5f41c43363e8bca8d757afb761362f9316
sparsh <sparshbhawsar@gmail.com>
2019-02-27 12:13:28 +0530

    First commit

---
+++ contacts/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.core.mail import send_mail
+from .models import Contact
+
+# Create your views here.
+def contact(request):
+    if request.method == "POST":
+        listing_id = request.POST['listing_id']
+        listing = request.POST['listing']
+        name = request.POST['name']
+        phone = request.POST['phone']
+        email = request.POST['email']
+        message = request.POST['message']
+        user_id = request.POST['user_id']
+        realtor_email = request.POST['realtor_email']
+        
+        if request.user.is_authenticated:
+            user_id = request.user.id
+            has_contacted = Contact.objects.all().filter(listing_id = listing_id, user_id = user_id)
+            if has_contacted:
+                messages.error(request,'you have already made an inquiry for this listing')
+                return redirect('/listings/'+listing_id)
+        contact = Contact(listing=listing, listing_id = listing_id, name = name, email= email, phone =phone, message=message, user_id = user_id)
+        contact.save()
+
+
+        # send mail
+        send_mail(
+            'Property listing inquiry',
+            'there has been an inquiry for '+listing +'. sign into the admin panel for more info',
+            'sparshbhawsar@gmail.com',
+            [realtor_email, 'sparshbhawsar@gmail.com'],
+            fail_silently = False
+        )
+
+        message.success(request, 'Your request has been submitted, a realtor will get back to you soon')
+        return redirect('/listings/'+listing_id)
---
+++ realtors/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Realtor
+class RealtorAdmin(admin.ModelAdmin):
+    list_display=('id','hire_date','email','name')
+    list_display_links=('id','name')
+    search_fields = ('name',)
+    list_per_page = 25
+admin.site.register(Realtor,RealtorAdmin)
